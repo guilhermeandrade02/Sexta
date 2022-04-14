9a32834679631f3b59ca4b60dbd0bb091d60a2fc
unknown <victorcals78@gmail.com>
2022-04-13 23:01:57 -0300

    projeto 1

---
+++ projeto_escola/js/jin.js
@@ -0,0 +1,17 @@
+function Esqueceu() {
+  window.location.href = 'cadastro/esqueceu.html'
+}
+
+function LoginCadastro() {
+  window.location.href = 'cadastro/CriarLogin.html'
+}
+
+// button mostrar senha
+function Toggle() {
+  var temp = document.getElementById('typepass')
+  if (temp.type === 'password') {
+    temp.type = 'text'
+  } else {
+    temp.type = 'password'
+  }
+}
